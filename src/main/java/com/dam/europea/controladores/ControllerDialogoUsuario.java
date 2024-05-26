b46f15fcfc53e9837fc5f0ae5886b13ef516317c
ainho <ainho@PCdeAinhoa>
2024-05-26 11:17:15 +0200

    Preparado dialogo Usuario crear y modificar

--- src/main/java/com/dam/europea/controladores/ControllerDialogoUsuario.java
+++ src/main/java/com/dam/europea/controladores/ControllerDialogoUsuario.java
@@ -1,24 +1,18 @@
 package com.dam.europea.controladores;
 
 import java.net.URL;
-import java.util.List;
 import java.util.ResourceBundle;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
-import com.dam.europea.entidades.Producto;
-import com.dam.europea.entidades.Proveedor;
 import com.dam.europea.entidades.Usuario;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -38,23 +32,28 @@ public class ControllerDialogoUsuario implements Initializable {
 	private Button btnAceptar;
 	@FXML
 	private Button btnCancelar;
+	
+	//Clases para rellenar
+	private Usuario u;
+	private ControllerGI_Users ct2;
 
-	public ControllerDialogoUsuario(SessionFactory sf, String iDUsuario) {
+	public ControllerDialogoUsuario(SessionFactory sf, String iDUsuario, ControllerGI_Users ct2) {
 		this.sf = sf;
-		IDUsuario = iDUsuario;
+		this.IDUsuario = iDUsuario;
+		this.ct2 = ct2;
 	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		session = sf.openSession();
+		session.beginTransaction();
 		if (IDUsuario != null) {
-			session.beginTransaction();
-			Usuario u = session.find(Usuario.class, IDUsuario);
+			u = session.find(Usuario.class, IDUsuario);
 			if (u != null) {
 				txtIDUsuario.setText(String.valueOf(u.getIdUsr()));
 				txtNombreUsuario.setText(u.getUserName());
 				txtContrasena.setText(u.getPass());
-				
+				;
 			}
 		}
 
@@ -63,7 +62,7 @@ public class ControllerDialogoUsuario implements Initializable {
 				if (IDUsuario == null) {
 					crearUsuario();
 				} else {
-					modFamiliaProducto();
+					modUsuario(u);
 				}
 				closeWindow();
 			} else {
@@ -97,36 +96,23 @@ public class ControllerDialogoUsuario implements Initializable {
 		session.getTransaction().commit();
 	}
 
-	public void modFamiliaProducto() {
-		ObservableList<Proveedor> proveedor = FXCollections.observableArrayList(); // Esto es lo que
-		// contiene los objetos
-		// que se muestran en el
-		// ComboBox
-
-		proveedor.add(new Proveedor("12312312", "CuadernosFinos S.L")); // Objetos de Ejemplo de las posibles familia de
-// Productos
-		proveedor.add(new Proveedor("63122312", "CuadernosGordos S.L"));
-		List<Producto> proveedorSelecionado = comboBoxProductos.getValue(); // Aquí sin embargo lo recojo como
-// List Producto para poder luego
-// hacer setProductosAsociados
-// SEGURAMENTE ESTAS LINEAS DE CÓDIGOS SE PUEDEN DECLARAR ARRIBA PARA ACCEDER
-// GLOBALMENTE Y NO CREARLO EN CADA MÉTODO, INVESTIGAR
-
-		Proveedor pv = new Proveedor();
-		pv.setCodigo(txtCodigoProveedor.getText());
-		pv.setNombre(txtNombreProveedor.getText());
-		pv.setProductosAsociados(proveedorSelecionado);
+	public void modUsuario(Usuario u) {
+		u = new Usuario();
+		u.setUserName(txtNombreUsuario.getText());
+		u.setPass(txtContrasena.getText());
 		session.beginTransaction();
-		session.merge(pv);
+		session.merge(u);
 		session.getTransaction().commit();
 	}
 
 	private void closeWindow() {
+		ct2.cargarTabla();
 		if (session != null && session.isOpen()) {
 			session.close();
 		}
 		Stage stage = (Stage) btnAceptar.getScene().getWindow();
 		stage.close();
+		
 	}
 
 }
