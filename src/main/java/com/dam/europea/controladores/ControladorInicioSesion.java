31fe22888e2775693fc344783c096eaa266e3dba
Angel Moldes Fernandez <angel26022000@gmail.com>
2024-06-09 11:18:23 +0200

    Añadido boton en la pagina de Inicio de Sesión que conecta con la guía web, unido botón con la ruta relativa del index.html en el controlador, correción estilos web

--- src/main/java/com/dam/europea/controladores/ControladorInicioSesion.java
+++ src/main/java/com/dam/europea/controladores/ControladorInicioSesion.java
@@ -1,7 +1,10 @@
 package com.dam.europea.controladores;
 
+import java.awt.Desktop;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -14,6 +17,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
@@ -21,6 +25,8 @@ import javafx.stage.Stage;
 public class ControladorInicioSesion implements Initializable {
 	@FXML
 	private ImageView imgView;
+	@FXML
+	private Button btnGuiaPaginaWeb;
 	private SessionFactory sf;
 
 	@Override
@@ -28,7 +34,27 @@ public class ControladorInicioSesion implements Initializable {
 		InputStream inputStream = getClass().getResourceAsStream("/iconoGrande.jpeg");
         Image imagen = new Image(inputStream);
 		imgView.setImage(imagen);
+		InputStream archivoPregunta= getClass().getResourceAsStream("/pregunta.png");
+		Image imagenPregunta= new Image(archivoPregunta, 35, 35, true, true);
+		btnGuiaPaginaWeb.setGraphic(new ImageView(imagenPregunta));
+		btnGuiaPaginaWeb.setOnAction(arg0 -> {
+			try {
+				abrirGuiaPaginaWeb(arg0);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
 	}
+	
+	public void abrirGuiaPaginaWeb(ActionEvent event) throws IOException, URISyntaxException {
+        // Recoge el directorio a la web y lo asigna
+        String htmlDirectorio = "/paginaWeb/index.html";
+        URI uri = ControladorInicioSesion.class.getResource(htmlDirectorio).toURI();
+
+        // Utiliza el navegador por defecto en el equipo
+        Desktop.getDesktop().browse(uri);
+    }
+
 	public void switchToMenu(ActionEvent event) throws IOException {
 	    FXMLLoader loader = new FXMLLoader(getClass().getResource("/erae.fxml"));
 	    Controller controller = new Controller(sf);
