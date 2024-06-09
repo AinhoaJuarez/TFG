42859d18471f36c7cf4261a21e90e52863d2c265
Angel Moldes Fernandez <angel26022000@gmail.com>
2024-06-09 13:37:01 +0200

    Creada nueva pantalla de Dialogo para datos empresa, con su respectivo para poder crear o modificar y una nueva entidad DatosEmpresa con los atributos necesarios

---
+++ src/main/java/com/dam/europea/entidades/DatosEmpresa.java
@@ -0,0 +1,104 @@
+package com.dam.europea.entidades;
+
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "datos empresa")
+public class DatosEmpresa {
+
+	@Id
+	private String nif;
+	@Column
+	private String nombreEmpresa;
+	private String nombreDueno;
+	private String direccion;
+	private String localidad;
+	private String codigoPostal;
+	
+	
+	public DatosEmpresa(String nif, String nombreEmpresa, String nombreDueno, String direccion, String localidad,
+			String codigoPostal) {
+		super();
+		this.nif = nif;
+		this.nombreEmpresa = nombreEmpresa;
+		this.nombreDueno = nombreDueno;
+		this.direccion = direccion;
+		this.localidad = localidad;
+		this.codigoPostal = codigoPostal;
+	}
+
+
+	public String getNif() {
+		return nif;
+	}
+
+
+	public void setNif(String nif) {
+		this.nif = nif;
+	}
+
+
+	public String getNombreEmpresa() {
+		return nombreEmpresa;
+	}
+
+
+	public void setNombreEmpresa(String nombreEmpresa) {
+		this.nombreEmpresa = nombreEmpresa;
+	}
+
+
+	public String getNombreDueno() {
+		return nombreDueno;
+	}
+
+
+	public void setNombreDueno(String nombreDueno) {
+		this.nombreDueno = nombreDueno;
+	}
+
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+
+	public String getLocalidad() {
+		return localidad;
+	}
+
+
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+
+
+	public String getCodigoPostal() {
+		return codigoPostal;
+	}
+
+
+	public void setCodigoPostal(String codigoPostal) {
+		this.codigoPostal = codigoPostal;
+	}
+
+
+	public DatosEmpresa() {
+		super();
+	}
+	
+	
+	
+	
+}
+
